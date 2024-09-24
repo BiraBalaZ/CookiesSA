3730968ceffa4656bf017b26d5e02ae59665a645
BiraBalaZ <erick.m.anjos@gmail.com>
2024-09-23 21:03:42 -0300

    Updating with more pages

--- PHP/admin_page.php
+++ PHP/admin_page.php
@@ -13,41 +13,64 @@ $result = $conn->query($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Administração</title>
+    <title>Cookies S.A. • Admin</title>
+    <link rel="stylesheet" href="../../CSS/style.css">
+    <link rel="stylesheet" href="../../CSS/inputs_style.css">
+    <link rel="stylesheet" href="../../CSS/footer.css">
+    <link rel="shortcut icon" href="../../Assets/Icons/s.a.ico" type="image/x-icon">
 </head>
 <body>
-    <h1>Página de Administração - Pedidos</h1>
+    <main>
+        <img id="logo" src="../Assets/Images/logo.png" alt="Logo da Cookies S.A.">
+        <h1>Página de Administração - Pedidos</h1>
 
-    <table border="1">
-        <tr>
-            <th>ID Pedido</th>
-            <th>Nome</th>
-            <th>CPF</th>
-            <th>Endereço</th>
-            <th>E-mail</th>
-            <th>Telefone</th>
-            <th>Cookies</th>
-            <th>Status</th>
-            <th>Data Pedido</th>
-            <th>Ações</th>
-        </tr>
-
-        <?php while ($row = $result->fetch_assoc()): ?>
+        <table border="1">
             <tr>
-                <td><?php echo $row['id']; ?></td>
-                <td><?php echo $row['nome']; ?></td>
-                <td><?php echo $row['cpf']; ?></td>
-                <td><?php echo $row['endereco']; ?></td>
-                <td><?php echo $row['email']; ?></td>
-                <td><?php echo $row['telefone']; ?></td>
-                <td><?php echo $row['cookies']; ?></td>
-                <td><?php echo $row['status']; ?></td>
-                <td><?php echo $row['data_pedido']; ?></td>
-                <td>
-                    <a href="editar_pedido.php?id=<?php echo $row['id']; ?>">Editar</a>
-                </td>
+                <th>ID Pedido</th>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>Endereço</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Cookies</th>
+                <th>Status</th>
+                <th>Data Pedido</th>
+                <th>Ações</th>
             </tr>
-        <?php endwhile; ?>
-    </table>
+            <tr>
+                <th>Nº Pedido</th>
+                <th>Situação</th>
+                <th>Dt. Recebimento</th>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>E-Mail</th>
+                <th>Endereço</th>
+                <th>Nº</th>
+                <th>Complemento</th>
+                <th>CEP</th>
+            </tr>
+
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['nome']; ?></td>
+                    <td><?php echo $row['cpf']; ?></td>
+                    <td><?php echo $row['endereco']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['telefone']; ?></td>
+                    <td><?php echo $row['cookies']; ?></td>
+                    <td><?php echo $row['status']; ?></td>
+                    <td><?php echo $row['data_pedido']; ?></td>
+                    <td>
+                        <a href="editar_pedido.php?id=<?php echo $row['id']; ?>">Editar</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+    </main>
+    <footer>
+        <p>Copyright &copy; <a href="#">Cookies S/A</a><br>Created by <a href="https://linktr.ee/birabalaz">Erick Monteiro</a></p>
+    </footer>
 </body>
 </html>
